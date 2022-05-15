f1e31d99e91803626df78c39e44b3211f179a2e3
EgitaKurpniece <egita.kurpniece@gmail.com>
2022-05-15 15:53:49 +0000

    Add another model, migration, factory, controller and seeder based on classwork example

---
+++ app/Models/Client.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\ClientFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'surname',
+        'email',
+        'address',
+    ];
+
+    protected static function newFactory(): ClientFactory
+    {
+        return new ClientFactory();
+    }
+}
